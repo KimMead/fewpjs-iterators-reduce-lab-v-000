e646e47ba25b25184b95ae3d1e7d2600eef788a8
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-12-27 23:55:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-function batteryCounter(totalAmount) {
-  return totalAmount;
-
-}
-
-
-}
+const totalBatteries =
+	batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 // Code your solution here
